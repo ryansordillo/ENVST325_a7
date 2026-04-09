77c083c0e28552e651ba157f3eecd2302b29458c
Ryan Sordillo <rsordill@hamilton.edu>
2026-04-09 15:17:02 +0000

    Tutorial Done

--- sordillo_activity07.R
+++ sordillo_activity07.R
@@ -3,3 +3,92 @@
 #Activity 07
 #ENVST325
 
+########Start of Tutorial################
+
+library(dplyr)
+library(ggplot2)
+library(olsrr)
+library(PerformanceAnalytics)
+
+# read in greenhouse gas data from reservoirs
+ghg <- read.csv("/cloud/project/activity07/Deemer_GHG_Data.csv")
+
+# log transform methane fluxes
+ghg$log.ch4 <- log(ghg$ch4+1)
+
+ghg$log.age <- log(ghg$age)
+ghg$log.DIP <- log(ghg$DIP+1)
+ghg$log.precip <- log(ghg$precipitation)
+
+unique(ghg$Region)
+
+# binary variable for boreal region
+ghg$BorealV <- ifelse(ghg$Region == "Boreal",1,0)
+# binary variable for tropical region
+ghg$TropicalV <- ifelse(ghg$Region == "Tropical",1,0)
+
+# binary variable for alpine region
+ghg$AlpineV <- ifelse(ghg$Alpine == "yes",1,0)
+
+# binary variable for known hydropower
+ghg$HydroV <- ifelse(ghg$hydropower == "yes",1,0)
+
+# multiple regression
+# creates a model object
+mod.full <- lm(log.ch4 ~ airTemp+
+                 log.age+mean.depth+
+                 log.DIP+
+                 log.precip+ BorealV, data=ghg) #uses the data argument to specify dataframe
+summary(mod.full)
+
+
+res.full <- rstandard(mod.full)
+fit.full <- fitted.values(mod.full)
+
+# qq plot
+qqnorm(res.full, pch=19, col="grey50")
+qqline(res.full)
+
+# shapiro-wilks test
+shapiro.test(res.full)
+
+plot(fit.full,res.full, pch=19, col="grey50")
+abline(h=0)
+
+# isolate continuous model variables into data frame:
+
+reg.data <- data.frame(ghg$airTemp,
+                       ghg$log.age,ghg$mean.depth,
+                       ghg$log.DIP,
+                       ghg$log.precip)
+
+# make a correlation matrix 
+chart.Correlation(reg.data, histogram=TRUE, pch=19)
+
+# run stepwise
+full.step <- ols_step_forward_aic(mod.full)
+# view table
+full.step 
+
+# check full model
+full.step$model
+
+# plot AIC over time
+plot(full.step )
+
+
+# prediction with interval for predicting a point
+predict.lm(mod.full, data.frame(airTemp=20,log.age=log(2),
+                                mean.depth=15,log.DIP=3,
+                                log.precip=6, BorealV=0),
+           interval="prediction")
+
+
+
+
+
+
+
+
+
+
